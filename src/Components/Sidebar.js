0ca7716cc1e8d78460271ee47766ea07c052b964
InnervisionGit <ivan.felbabic@gmail.com>
2020-12-12 17:45:00 +0100

    Sidebar init

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Sidebar.css';
+import SidebarRow from './SidebarRow';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags';
+import PeopleIcon from '@material-ui/icons/People';
+import ChatIcon from '@material-ui/icons/Chat';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import ExpandMoreOutlined from '@material-ui/icons/ExpandMoreOutlined';
+import { useStateValue } from '../StateProvider';
+
+const Sidebar = () => {
+  //const [{user},dispatch]=useStateValue()
+
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default Sidebar;
