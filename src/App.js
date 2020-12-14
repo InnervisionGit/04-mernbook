90d75d2c13ceddcdc3822561ab5cf9a793a3f43f
InnervisionGit <ivan.felbabic@gmail.com>
2020-12-14 12:26:05 +0100

    sidebar done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './Components/Header.js';
+import Sidebar from './Components/Sidebar.js';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <h2>MongoDB: 04-mernbook</h2>
       <h1>9LxCDH1V5IJ3Sy0c</h1>
       <Header />
-      {/* sidebar */}
+      <Sidebar />
       {/* feed */}
       {/* widgets */}
     </div>
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -8,14 +8,17 @@ import ChatIcon from '@material-ui/icons/Chat';
 import StorefrontIcon from '@material-ui/icons/Storefront';
 import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
 import ExpandMoreOutlined from '@material-ui/icons/ExpandMoreOutlined';
-import { useStateValue } from '../StateProvider';
+// import { useStateValue } from '../StateProvider';
 
 const Sidebar = () => {
   //const [{user},dispatch]=useStateValue()
 
   return (
-    <div>
-      <SidebarRow src='' title='Inner' />
+    <div className='sidebar'>
+      <SidebarRow
+        src='https://upload.wikimedia.org/wikipedia/en/5/55/SHODAN_hires.jpg'
+        title='Innervision'
+      />
       <SidebarRow Icon={LocalHospitalIcon} title='COVID-19 Info' />
       <SidebarRow Icon={EmojiFlagsIcon} title='Pages' />
       <SidebarRow Icon={PeopleIcon} title='Friends' />
@@ -28,3 +31,4 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+// Collapse;
